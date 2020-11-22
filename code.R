b88ebb9b4c39ba84ff45f602d28ba4c466e06a35
abdumalik2014 <abdumalik2014@gmail.com>
2020-11-22 12:19:37 +0100

    add headings and date

--- code.R
+++ code.R
@@ -14,3 +14,62 @@ headings <-
   html_nodes('.c-entry-box--compact__title') %>% 
   html_text()
 headings
+
+posted_date <- 
+  file_html %>% 
+  html_nodes('time') %>% 
+  html_text(trim = TRUE)
+
+# posted_date <- 
+#  file_html %>% 
+#  html_nodes('.c-byline__item span') %>% 
+#  html_text() 
+
+posted_date
+
+dates <- file_html %>% 
+  html_nodes('time') %>% 
+  html_attrs() %>% 
+  map(1) %>% 
+  # Parse the string into a datetime object with lubridate
+  ymd_hms() %>%                 
+  unlist()
+
+
+author <- 
+  file_html %>% 
+  html_nodes('.c-byline__author-name') %>% 
+  html_text()
+author
+
+
+author <- 
+  file_html %>% 
+  html_nodes('.c-byline__item') %>% 
+  html_text(trim = TRUE) 
+author
+
+
+
+
+
+author <- 
+  file_html %>% 
+  html_nodes('.c-byline__item') %>% 
+  .[[2]] %>% 
+  html_text() 
+
+
+results <- file_html %>% html_nodes('.c-byline__item')
+
+second_result <- results[2]
+second_result %>% html_nodes("time")
+date <- second_result %>% html_nodes("time") %>% html_text(trim = TRUE)
+
+xml_contents(second_result)
+
+
+# alternative for trim = TRUE:
+# author <- gsub("[\n]", "", author)
+# author <- trimws(author)
+# if date contains Updated, remove Updated
