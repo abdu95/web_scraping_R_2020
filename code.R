9073016d513f196b110831614ea6c4afdaee8131
abdumalik2014 <abdumalik2014@gmail.com>
2020-11-23 10:33:45 +0100

    add markdown

--- code.R
+++ code.R
@@ -1,66 +1,58 @@
+# news website - vox.com
+
 library(rvest)
 library(data.table)
 
-rm(list=ls())
-
-# to get a web page, pass URL to read_html 
-file_html <- read_html('https://www.vox.com/search?page=1&q=Biden')
-
-# save as html file
-write_html(file_html, 'index.html')
-
-headings <- 
-  file_html %>% 
-  html_nodes('.c-entry-box--compact__title') %>% 
-  html_text()
-headings
-
-post_links <- 
-  file_html %>% 
-  html_nodes('.c-entry-box--compact__title') %>% 
-  html_nodes('a') %>%
-  html_attr('href')
-
-posted_date <- 
-  file_html %>% 
-  html_nodes('time') %>% 
-  html_text(trim = TRUE)
-
-#dates <- file_html %>% 
-#  html_nodes('time') %>% 
-#  html_attrs() %>% 
-#  map(1) %>% 
-#  # Parse the string into a datetime object with lubridate
-#  ymd_hms() %>%                 
-#  unlist()
-
-author <- 
-  file_html %>% 
-  html_nodes('.c-byline__item') %>% 
-  html_text(trim = TRUE) 
-
-authors <- c()
-
-for(x in seq(1,length(author),3)) {
-  authors <- c(authors, author[x]) 
+# a function which downloads information from the website to dataframe
+get_one_page_from_vox <- function(arg_url) {
+  
+  # to get a web page, pass URL to read_html 
+  file_html <- read_html(arg_url)
+  
+  # save it as html document
+  write_html(file_html, 'html_file.html')
+  
+  headings <- 
+    file_html %>% 
+    html_nodes('.c-entry-box--compact__title') %>% 
+    html_text()
+  
+  post_links <- 
+    file_html %>% 
+    html_nodes('.c-entry-box--compact__title') %>% 
+    html_nodes('a') %>%
+    html_attr('href')
+  
+  posted_date <- 
+    file_html %>% 
+    html_nodes('time') %>% 
+    html_text(trim = TRUE)
+  
+  author <- 
+    file_html %>% 
+    html_nodes('.c-byline__item') %>% 
+    html_text(trim = TRUE) 
+  
+  authors <- c()
+  
+  for(x in seq(1,length(author),3)) {
+    authors <- c(authors, author[x]) 
+  }
+  
+  # to create a dataframe
+  df <- data.frame('headings' = headings, 'post_link' = post_links, 'date' = posted_date, 'author' = authors)  
+  
+  return(df)
 }
 
-# to create a df
-df <- data.frame('headings' = headings, 'post_link' = post_links, 'date' = posted_date, 'author' = authors)  
+# test the function
+test_df <- get_one_page_from_vox('https://www.vox.com/search?page=1&q=Biden')
 
+# generate the URLs
+test_urls <- paste0('https://www.vox.com/search?page=', 1:5, '&q=Biden' )
 
+#apply a function to a list
+df_list <- lapply(test_urls, get_one_page_from_vox)
 
-results <- file_html %>% html_nodes('.c-byline__item')
-
-second_result <- results[2]
-second_result %>% html_nodes("time")
-date <- second_result %>% html_nodes("time") %>% html_text(trim = TRUE)
-
-xml_contents(second_result)
-
-
-
-# alternative for trim = TRUE:
-# author <- gsub("[\n]", "", author)
-# author <- trimws(author)
-# if date contains Updated, remove Updated
+#create a data.table object of the list of data.frames
+df <- rbindlist(df_list)
