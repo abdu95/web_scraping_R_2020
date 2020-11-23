ed48e01bbb981f84491bbae2bf98975fbb905611
abdumalik2014 <abdumalik2014@gmail.com>
2020-11-23 10:59:30 +0100

    update docs

--- code_2.R
+++ code_2.R
@@ -1,5 +1,6 @@
 #' Create a function which requires two arguments. 
 #' First a keyword then a number of pages to download.
+library(readr)
 
 get_vox_pages <- function(keyword, no_pages){
   main_url <- 'https://www.vox.com/search?page='
@@ -7,6 +8,11 @@ get_vox_pages <- function(keyword, no_pages){
   # generate the URLs
   links <- paste0(main_url, 1:no_pages, '&q=', keyword)
   return_df <- rbindlist(lapply(links, get_one_page_from_vox))
+  
+  write_csv(return_df, 'data/result.csv')
+  saveRDS(return_df, file = 'data/rdsfile.rds')
+  
+  return(return_df)
 }
 
 # check the function
